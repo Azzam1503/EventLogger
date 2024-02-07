d120bf29e6dd651f29bdf1f91654f6663c8feb15
AZZAM UDDIN <uazzam69@gmail.com>
2024-02-07 23:30:26 +0530

    fixed event routes

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -4,7 +4,7 @@ const eventController = require("../controllers/eventController");
 
 router.get("/", eventController.allEvents);
 router.post("/create-event", eventController.craeteEvent);
-router.put("/update-event/:id", eventController.craeteEvent);
+router.put("/update-event/:id", eventController.updateEvent);
 router.delete("/delete-event/:id", eventController.deleteEvent);
 
 module.exports = router;
