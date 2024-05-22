63e6c8962fd49eff2dc5532cedb87e8f0b91ce3f
Vivek Gupta <vivekgupta4115@gmail.com>
2024-05-22 15:12:51 +0530

    update navbar home and login page

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-// import Images from "../assets/images/playimges.jpg"
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
-import fromeImage from '../assets/images/games22.jpg'
+import Images from "../assets/images/mjpru1.jpg"
+import fromeImage from '../assets/images/frame.png'
 
 const Login = ({isLoggedIn, setIsLoggedIn}) => {
   const navigate = useNavigate();
